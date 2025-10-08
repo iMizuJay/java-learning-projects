543e7b152dfc32ecd87dce7183ee4291510e68c0
Jabralin M <jabralinmckinney@gmail.com>
2025-10-07 21:35:23 -0500

    Adding my first project in github

---
+++ PlantingATree/PlantingATree.java
@@ -0,0 +1,16 @@
+package PlantingATree;
+
+public class PlantingATree {
+    public static void main(String[] arg) {
+        // We're going to introduce ourselves and print a tree to the screen.
+        System.out.println("Hey there, I'm Jabralin and I'm learning to code in Java!");
+        System.out.println("I'm going to plant a tree today! I'm ready to get my hands dirty!");
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("    *    ");
+        System.out.println("    *    ");
+        System.out.println("_________");
+    }
+}
---
+++ PlantingATree/README.md
@@ -0,0 +1,3 @@
+# Planting a Tree 🌳
+A simple Java program that prints a small tree to the console.  
+This project helped me practice `System.out.println()` and basic syntax.
