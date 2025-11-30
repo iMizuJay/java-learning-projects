4a27fdd902d60017715ebc744ef56b7792d5b0de
Jabralin M <jabralinmckinney@gmail.com>
2025-11-29 19:18:24 -0600

    Add MadLibs and MathMagic Java projects

---
+++ MadLibs/README.md
@@ -0,0 +1,2 @@
+Concepts: Variables, string concatenation.
+What I Learned: Combining user input/data to generate dynamic output.
---
+++ MathMagic/MathMagic.java
@@ -0,0 +1,20 @@
+package MathMagic;
+
+public class MathMagic {
+    public static void main(String[] args) {
+    //This is the original number
+    int myNumber = 1738;
+
+    int stepOne = myNumber * myNumber;
+    int stepTwo = stepOne + myNumber;
+    int stepThree = stepTwo / myNumber;
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour - myNumber;
+    int stepSix = stepFive / 6;
+    
+    System.out.println(stepSix);
+
+		
+
+	}
+}
